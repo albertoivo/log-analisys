6191b2406a693600059ebb178b0f2432ebd8ef0b
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-04-28 14:45:54 -0300

    Main file to call all other python queries.

---
+++ main.py
@@ -0,0 +1,8 @@
+import error as error
+import popularAuthors as authors
+import popularArticles as articles
+
+if __name__ == '__main__':
+    articles.connect()
+    authors.connect()
+    error.connect()
