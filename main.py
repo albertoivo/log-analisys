7baa80d7772728fd822bb229a81d69f723ef4a3b
Alberto <alberto.vieira@serpro.gov.br>
2018-11-07 10:19:42 -0200

    refactoring for new directories

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import error as error
-import popularAuthors as authors
-import popularArticles as articles
+import sql_queries.error as error
+import sql_queries.popularAuthors as authors
+import sql_queries.popularArticles as articles
 
 if __name__ == '__main__':
     articles.connect()
